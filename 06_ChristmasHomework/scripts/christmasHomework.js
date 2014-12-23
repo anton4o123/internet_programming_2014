828940b24228f84245dbb391cdcdff5d4c377913
Anton Georgiev <anton4o123@abv.bg>
2014-12-23 12:45:30 +0200

    Selecting an element and adding a child element with text to it - Task 4

--- 06_ChristmasHomework/scripts/christmasHomework.js
+++ 06_ChristmasHomework/scripts/christmasHomework.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
 	console.log($(".tu").attr("title"));
 	console.log($("#col1 p").text());
+
+	$("#menu-top-level-menu").append($("<li/>").append($("<a/>").text("new button")));
 });
