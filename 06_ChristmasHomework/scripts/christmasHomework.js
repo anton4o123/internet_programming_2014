523b8e01d15a1d36313b4d9fc514841121ced474
Anton Georgiev <anton4o123@abv.bg>
2014-12-23 13:55:21 +0200

    Getting posts with ajax request and putting them in the unordered list - Task 11

--- 06_ChristmasHomework/scripts/christmasHomework.js
+++ 06_ChristmasHomework/scripts/christmasHomework.js
@@ -13,4 +13,27 @@ $(document).ready(function() {
 		alert("hello world");
 		$("#col2").insertBefore($("#col1"));
 	});
+
+	function handleError(error) {
+		console.error("error", error, arguments);
+	}
+	function appendToList(list, post) {
+		var newElement = $("<li/>");
+		newElement.text(post.title);
+		list.append(newElement);
+	}
+	function processResponse(response) {
+		var list = $("ul#posts");
+		var i = 0;
+
+		$.each(response, function() {
+			appendToList(list, this);
+			if(++i >= 5) {
+				return false;
+			}
+		});
+	}
+	$.ajax("http://jsonplaceholder.typicode.com/posts", {
+		method: "GET"
+	}).then(processResponse, handleError);
 });
